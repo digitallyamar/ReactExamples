c61b21c075b0ffd278830745fd21ed599b812f05
digitallyamar <digitallyamar@gmail.com>
2021-06-28 17:26:01 +0530

    Working examples from Mosh course

---
+++ reactMosh/vidly/src/components/movies.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (id) => {
+    const newMovies = this.state.movies.filter((movie) => movie._id !== id);
+
+    this.setState({
+      movies: newMovies,
+    });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    type="submit"
+                    onClick={() => this.handleDelete(movie._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
