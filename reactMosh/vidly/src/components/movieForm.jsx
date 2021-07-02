e4c2c068109ef6780000f4b22828ae79bfbf5438
digitallyamar <digitallyamar@gmail.com>
2021-07-02 12:42:54 +0530

    Working until Chapter:7 - Forms

--- reactMosh/vidly/src/components/movieForm.jsx
+++ reactMosh/vidly/src/components/movieForm.jsx
@@ -1,17 +1,98 @@
 import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
 
-const MovieForm = ({ match, history }) => {
-  return (
-    <div>
-      <h1>Movie Form {match.params.id} </h1>
-      <button
-        className="btn btn-primary"
-        onClick={() => history.push("/movies")}
-      >
-        Save
-      </button>
-    </div>
-  );
-};
+import { saveMovie, getMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
+
+class MovieForm extends Form {
+  state = {
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: "",
+    },
+    genres: [],
+    errors: {},
+  };
+
+  schema = {
+    _id: Joi.string(),
+    title: Joi.string().required().label("Title"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.number()
+      .min(0)
+      .max(100)
+      .required()
+      .label("Number In Stock"),
+    dailyRentalRate: Joi.number()
+      .min(0)
+      .max(10)
+      .required()
+      .label("Daily Rental Rate"),
+  };
+
+  handleSelectChange = (e) => {
+    const data = this.state.data;
+    if (e.target.value === "Action") {
+      data.genre._id = "5b21ca3eeb7f6fbccd471818";
+      data.genre.name = e.target.value;
+    } else if (e.target.value === "Comedy") {
+      data.genre._id = "5b21ca3eeb7f6fbccd471814";
+      data.genre.name = e.target.value;
+    } else if (e.target.value === "Thriller") {
+      data.genre._id = "5b21ca3eeb7f6fbccd471820";
+      data.genre.name = e.target.value;
+    }
+
+    this.setState(data);
+    console.log("handleSelectChange++", this.state.data);
+  };
+
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
+
+    const movieId = this.props.match.params.id;
+
+    if (movieId === "new") return;
+
+    const movie = getMovie(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+
+    this.setState({ data: this.mapToViewModel(movie) });
+  }
+
+  mapToViewModel(movie) {
+    return {
+      _id: movie._id,
+      title: movie.title,
+      genreId: movie.genre._id,
+      numberInStock: movie.numberInStock,
+      dailyRentalRate: movie.dailyRentalRate,
+    };
+  }
+
+  doSubmit = () => {
+    saveMovie(this.state.data);
+    this.props.history.push("/movies");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Movie Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderInput("numberInStock", "Number In Stock", "number")}
+          {this.renderInput("dailyRentalRate", "Rate")}
+          {this.renderButton("Save")}
+        </form>
+      </div>
+    );
+  }
+}
 
 export default MovieForm;
--- reactMosh/vidly/src/components/movies.jsx
+++ reactMosh/vidly/src/components/movies.jsx
@@ -8,6 +8,9 @@ import { getGenres } from "../services/fakeGenreService";
 import MoviesTable from "./moviesTable";
 
 import _ from "lodash";
+import { Link } from "react-router-dom";
+import Input from "./common/input";
+import SearchBox from "./common/searchBox";
 
 class Movies extends Component {
   state = {
@@ -15,6 +18,8 @@ class Movies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    searchQuery: "",
+    selectedGenre: null,
     sortColumn: { path: "title", order: "asc" },
   };
 
@@ -45,11 +50,15 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre, currentPage: 1 });
+    this.setState({ selectedGenre: genre, searchQuery: "", currentPage: 1 });
   };
 
   handleSort = (sortColumn) => {
     this.setState({ sortColumn });
+  };
+
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
   };
 
   getPagedData = () => {
@@ -58,13 +67,18 @@ class Movies extends Component {
       currentPage,
       sortColumn,
       selectedGenre,
+      searchQuery,
       movies: allMovies,
     } = this.state;
 
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-        : allMovies;
+    let filtered = allMovies;
+
+    if (searchQuery)
+      filtered = allMovies.filter((m) =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter((m) => m.genre._id === selectedGenre._id);
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
@@ -75,9 +89,21 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, sortColumn } = this.state;
+    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
 
-    if (count === 0) return <p>There are no movies in the database.</p>;
+    if (count === 0)
+      return (
+        <div>
+          <p>There are no movies in the database.</p>
+          <Input
+            type="text"
+            name="search"
+            value={this.state.search}
+            placeholder="Search..."
+            onChange={this.handleSearch}
+          />
+        </div>
+      );
 
     const { totalCount, data: movies } = this.getPagedData();
 
@@ -91,7 +117,11 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
+          <Link to="/movies/new" className="btn btn-primary mb-4">
+            New Movie
+          </Link>
           <p>Showing {totalCount} movies in the database.</p>
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
